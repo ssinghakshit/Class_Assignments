e5f5029346584877fdeedc969e6e47922fa91d9a
Akshit Singh <akshit.singh2002@gmail.com>
2021-09-30 18:15:29 +0545

    POINTER

--- 2nd Sem/C Codes/Assignment 1/1.1.c
+++ 2nd Sem/C Codes/Assignment 1/1.1.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 void main()
 {
-	printf("My Roll No. is: 20051762\n");
-	printf("My name is Anas\n");
+	printf("My Roll No. is: 838562385\n");
+	printf("My name is Rajesh hamal\n");
 	printf("I am from CSE\n");
+	int b=87;
+	int *p=&b;
+	printf("%d %d".b,p);
 }
