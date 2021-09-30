5ee0e607c593339863beeb8b54dcf5952436b1ef
Akshit Singh <akshit.singh2002@gmail.com>
2021-09-30 18:30:25 +0545

    logic changed
    logic changed and variable modification

--- 2nd Sem/C++ Codes/Part 10/2.cpp
+++ 2nd Sem/C++ Codes/Part 10/2.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int x;
     cout << "Enter number of rows: ";
-    cin >> n;
+    cin >> x;
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i < x; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            cout << "* ";
+            cout << "* *  ";
         }
         cout << endl;
     }
